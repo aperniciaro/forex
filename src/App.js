4d704077c59192bf0c7f6fd0ea8253b585892106
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-03-06 13:59:12 -0500

    importing current and historical api data sets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import axios from 'axios'
 
 class App extends Component {
+  state = {
+    currentUrl: 'https://api.exchangeratesapi.io/latest?base=USD',
+    historicalUrl:
+      'https://api.exchangeratesapi.io/history?start_at=2009-03-06&end_at=2019-03-06'
+  }
+
+  componentDidMount() {
+    axios.get(this.state.currentUrl).then(resp => {
+      console.log({ resp })
+    })
+    axios.get(this.state.historicalUrl).then(resp => {
+      console.log({ resp })
+    })
+  }
+
   render() {
-    return <HelloWorld />
+    return (
+      <>
+        <h1>Currency Trader</h1>
+        <select className="from-currency" />
+        <select className="to-currency" />
+      </>
+    )
   }
 }
 
