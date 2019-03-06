90fea12b75171d402c4772de3425da1ac990b61c
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-03-06 14:17:39 -0500

    full layout

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   state = {
     currentUrl: 'https://api.exchangeratesapi.io/latest?base=USD',
     historicalUrl:
-      'https://api.exchangeratesapi.io/history?start_at=2009-03-06&end_at=2019-03-06'
+      'https://api.exchangeratesapi.io/history?start_at=2009-03-06&end_at=2019-03-06&base=USD'
   }
 
   componentDidMount() {
@@ -20,9 +20,35 @@ class App extends Component {
   render() {
     return (
       <>
-        <h1>Currency Trader</h1>
-        <select className="from-currency" />
-        <select className="to-currency" />
+        <header>
+          <h1>Currency Trader</h1>
+        </header>
+        <section className="menus">
+          <select className="from-currency" />
+          <select className="to-currency" />
+        </section>
+        <section className="current-rate">
+          <p>{} trading at </p>
+          <p>
+            {} {}
+          </p>
+        </section>
+        <section className="historical-rates">
+          <h5>1 year average: </h5>
+          <p className="one-yr-avg">
+            {} {}
+          </p>
+          <h5>10 year average: </h5>
+          <p className="ten-yr-avg">
+            {} {}
+          </p>
+        </section>
+        <section className="explanation">
+          <p className="green-text">Green: </p>
+          <p>Trading below average</p>
+          <p className="red-text">Red: </p>
+          <p>Trading above average</p>
+        </section>
       </>
     )
   }
